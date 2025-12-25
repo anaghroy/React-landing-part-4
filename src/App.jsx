e2228bb03f8728d7e33d82b6ae278d53c47a002a
AnaghRoy <royanagh1999@gmail.com>
2025-12-25 17:08:02 +0530

    Shortest Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar'
 import Rank from './Components/Rank'
 import Seamless from './Components/Seamless'
 import Services from './Components/Services'
+import Shortest from './Components/Shortest'
 import { logo } from './Data/BrandImage'
 import { term } from './Data/CapabiliData'
 import { tableData } from './Data/Table'
@@ -15,6 +16,7 @@ const App = () => {
       <Seamless tableData={tableData}/>
       <Services/>
       <Capabilities term={term}/>
+      <Shortest/>
     </div>
   )
 }
---
+++ src/Components/Shortest.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Shortest = () => {
+  return (
+    <div className="main-short">
+      <div className="text">
+        <h1>Find the <span>shortest path</span></h1>
+        <h1>to revenue with Entlify</h1>
+      </div>
+      <div className="button">
+        <a href="#">Request a Call</a>
+      </div>
+    </div>
+  );
+};
+
+export default Shortest;
